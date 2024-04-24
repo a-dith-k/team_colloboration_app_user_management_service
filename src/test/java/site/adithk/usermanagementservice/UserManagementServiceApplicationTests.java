0f40c22a2d27b9679858e8dfefb6c941d523df90
a-dith-k <adithk1308@gmail.com>
2024-04-24 22:32:57 +0530

    initial commit

---
+++ src/test/java/site/adithk/usermanagementservice/UserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package site.adithk.usermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
